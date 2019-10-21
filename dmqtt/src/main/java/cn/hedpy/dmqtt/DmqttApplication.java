2fa72f33d8b84734276124c6c459b9d6768fc206
dpy615 <dpy615@163.com>
2019-10-21 21:57:43 +0800

    导入初始

---
+++ dmqtt/src/main/java/cn/hedpy/dmqtt/DmqttApplication.java
@@ -0,0 +1,13 @@
+package cn.hedpy.dmqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmqttApplication.class, args);
+	}
+
+}
---
+++ dmqtt/src/main/java/cn/hedpy/dmqtt/net/NettyApplication.java
@@ -0,0 +1,27 @@
+package cn.hedpy.dmqtt.net;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class NettyApplication implements ApplicationRunner{
+
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup works = new NioEventLoopGroup();
+		
+		ServerBootstrap sbs = new ServerBootstrap();
+		sbs.group(bossGroup,works);
+	}
+
+}
